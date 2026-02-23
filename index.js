4475a1bfaef137e88583789f942053b801599d7d
louith <sepradoloys@gmail.com>
2026-02-23 18:20:59 +0800

    feat: dom events

---
+++ index.js
@@ -0,0 +1,53 @@
+// MouseEvent
+const clickBtn = document.getElementById("clickBtn");
+
+clickBtn.addEventListener("mousedown", (e) => {
+  console.log("MouseEvent", e.type);
+});
+
+// clickBtn.addEventListener("contextmenu", (e) => {
+//   console.log("MouseEvent", e.type);
+// });
+
+// clickBtn.addEventListener("mousemove", (e) => {
+//   console.log("MouseEvent", e.type);
+// });
+
+// clickBtn.addEventListener("click", (e) => {
+//   console.log("MouseEvent", e.pointerType);
+// });
+
+// KeyboardEvent
+const textInput = document.getElementById("textInput");
+textInput.addEventListener("keydown", (e) => {
+  console.log("KeyboardEvent", e.constructor.name);
+});
+textInput.addEventListener("input", (e) => {
+  console.log("InputEvent", e.constructor.name);
+});
+
+// FocusEvent: focus and blur
+textInput.addEventListener("focus", (e) => {
+  console.log("FocusEvent focus", e.constructor.name);
+});
+textInput.addEventListener("blur", (e) => {
+  console.log("FocusEvent blur", e.constructor.name);
+});
+
+// PointerEvent: clicking / touching div
+const hoverBox = document.getElementById("hoverBox");
+hoverBox.addEventListener("pointerdown", (e) => {
+  console.log("PointerEvent", e.constructor.name);
+});
+
+// CustomEvent: triggered by custom button
+const customBtn = document.getElementById("customBtn");
+document.addEventListener("userLoggedIn", (e) => {
+  console.log("CustomEvent", e.constructor.name, "detail:", e.detail);
+});
+customBtn.addEventListener("click", () => {
+  const event = new CustomEvent("userLoggedIn", {
+    detail: { username: "student123" },
+  });
+  document.dispatchEvent(event);
+});
