355db0ff8db8118a771bc2b9857cdf6094b70ac2
louith <sepradoloys@gmail.com>
2026-03-25 19:47:29 +0800

    feat: initial setup + crud requests

---
+++ server.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const users = require("./users.js");
+const app = express();
+
+app.use(express.json()); // IMPORTANT: parse JSON body
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+// post request
+app.post("/users", (req, res) => {
+  const newUser = {
+    id: users.length + 1,
+    name: req.body.name,
+  };
+
+  users.push(newUser);
+
+  res.status(201).json(newUser);
+});
+
+// put request
+app.put("/users/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+
+  const index = users.findIndex((u) => u.id === id);
+
+  if (index === -1) {
+    return res.status(404).json({ message: "User not found" });
+  }
+
+  users[index] = {
+    id: id,
+    name: req.body.name,
+  };
+
+  res.json(users[index]);
+});
+
+// patch (partial update/s)
+app.patch("/users/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+
+  const user = users.find((u) => u.id === id);
+
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+
+  if (req.body.name !== undefined) {
+    user.name = req.body.name;
+  }
+
+  res.json(user);
+});
+
+// delete
+app.delete("/users/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+
+  const index = users.findIndex((u) => u.id === id);
+
+  if (index === -1) {
+    return res.status(404).json({ message: "User not found" });
+  }
+
+  users.splice(index, 1);
+
+  res.json({ message: "User deleted" });
+});
---
+++ users.js
@@ -0,0 +1,6 @@
+let users = [
+  { id: 1, name: "John" },
+  { id: 2, name: "Jane" },
+];
+
+module.exports = users;
